5ac1c063e2d6997bbbfbc852c8dba3da5b0d372a
sanky2020 <sankyk2020@gmail.com>
2022-03-07 12:51:52 +0530

    converted userInput value to Number type in action.payload

--- src/reducers/amountReducer.js
+++ src/reducers/amountReducer.js
@@ -1,10 +1,10 @@
 const reducer = (state=1000, action) => {
     switch (action.type){
         case 'DEPOSIT': {
-            return state + action.payload
+            return state + Number(action.payload)
         }
         case 'WITHDRAW':{
-            return state - action.payload
+            return state - Number(action.payload)
         }
         default : return state
     }
